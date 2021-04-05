649115ad62bc0c593f3b9ff231c5208daf59d75d
lubovgolikova <lubovgolikova@gmail.com>
2021-04-05 14:05:02 +0300

    add AnswerRepository

--- src/app/Repositories/AnswerRepository.php
+++ src/app/Repositories/AnswerRepository.php
@@ -16,14 +16,7 @@ class AnswerRepository
 
     public function create($data)
     {
-        $answer = new Answer;
-        $user = JWTAuth::parseToken()->authenticate();
-        $answer->user_id = $user->id;
-        $answer->question_id = $data['question_id'];
-        $answer->body = $data['body'];
-        $answer->votes = $data['votes'];
-        $answer->save();
-        return $answer;
+        return Answer::create($data);
     }
 
     public function addVote($id)
--- src/app/Services/AnswerService.php
+++ src/app/Services/AnswerService.php
@@ -3,6 +3,7 @@
 namespace  App\Services;
 use App\Models\Answer;
 use App\Repositories\AnswerRepository;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AnswerService
 {
@@ -20,6 +21,11 @@ class AnswerService
 
     public function create($data)
     {
+        $arrElc = [];
+        $user = JWTAuth::parseToken()->authenticate();
+        $arrElc['user_id'] = $user->id;
+        $arrElc['question_id'] = $data['question_id'];
+        $arrElc['body'] = $data['body'];
         return $this->answerRepository->create($data);
     }
 
