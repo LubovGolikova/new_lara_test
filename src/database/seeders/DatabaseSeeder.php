f1ec3f759891f10bd8daf6dc30cdac3b5f9561f0
lubovgolikova <lubovgolikova@gmail.com>
2021-04-07 17:58:40 +0300

    delete role admin in seeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        \DB::table('roles')->insert(['name' => 'user']);
-//        \DB::table('roles')->insert(['name' => 'admin']);
 //        \App\Models\User::factory(10)->create();
 //        \App\Models\Question::factory(10)->create();
 //        \App\Models\Answer::factory(10)->create();
