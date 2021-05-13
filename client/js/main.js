1d02591f7478a9a363ed92178f5a989d6fa6b7ed
lubovgolikova <lubovgolikova@gmail.com>
2021-05-13 18:01:55 +0300

    add answers

--- client/js/main.js
+++ client/js/main.js
@@ -2,15 +2,17 @@
     $(document).ready(function() {
         $.get("http://127.0.0.1:8000/api/questions", function(data, status){
             var bodyString = '';
-            data.forEach(({ id, user_id, title, body, votes_questions_count, }) => {
-
-                bodyString += ('<div class="row"><div class="stats-container col-1 ">' +
-                    '<p>' + id + '</p>' + '<p>' + votes_questions_count + '</p>' + '</div>'
+            data.forEach(({ user_id, title, body, votes_questions_count}) => {
+                var a=0;
+                bodyString += ('<div class="row"><div class="stats-container col-1">' +
+                    '<p>' + votes_questions_count +'</p>'+ '<label>votes</label>' +
+                    '<p>' + a +'</p>'+ '<label>answers</label>'+
+                    '</div>'
                     + '<div class="summary">'+
                     // '<div class="question-container col-11">'+'<a href="../view/question.html/'+id+'\"'+'><h2>' + title + '</h2></a>'
                     '<div class="question-container col-11">'+'<a href="../view/question.html"><h2>' + title + '</h2></a>'
                     +'<p>' + body + '</p>' + '</div>' + '<div class="user-container">'+ user_id +
-                    '</div>'+'</div>'+'</div>'+'</div>'+'<div class="separator"></div>');
+                    '</div>'+'</div>'+'</div>'+'<div class="separator"></div>');
 
                 $('.question-summary').html(bodyString);
             })
