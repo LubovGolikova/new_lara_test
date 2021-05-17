33129dbea7cfa220cf2d69ae7d47f67f1e6d8cc6
lubovgolikova <lubovgolikova@gmail.com>
2021-05-17 18:26:42 +0300

    add some changes

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('layouts.home');
 });
 
 Route::get('/new-question', function () {
